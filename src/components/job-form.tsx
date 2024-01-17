76770de943378e99de86df6388f9ae1e1f3c3a30
aka <aaronkaltman@gmail.com>
2024-01-17 08:34:31 -0500

    locations drop down pretty cool

--- src/components/job-form.tsx
+++ src/components/job-form.tsx
@@ -1,12 +1,23 @@
 import { Input } from "./ui/input";
 import { Label } from "./ui/label";
 import Select from "./ui/select";
+import prisma from "@/lib/prisma";
 
 async function filterJobs() {
   "use server";
 }
 
 export default async function JobForm() {
+  const distinctLocations = (await prisma.job
+    .findMany({
+      where: { approved: true },
+      select: { location: true },
+      distinct: ["location"],
+    })
+    .then((locations) =>
+      locations.map((location) => location.location).filter(Boolean)
+    )) as string[];
+
   return (
     <div className="">
       <form action={filterJobs} className="space-y-4">
@@ -17,7 +28,7 @@ export default async function JobForm() {
           <Input id="q" name="q" type="text" placeholder="Search Jobs..." />
         </div>
         <div>
-          <Select />
+          <Select distinctLocations={distinctLocations} />
         </div>
       </form>
     </div>
--- src/components/ui/select.tsx
+++ src/components/ui/select.tsx
@@ -1,4 +1,8 @@
-export default function Select() {
+type Props = {
+  distinctLocations: string[];
+};
+
+export default function Select({ distinctLocations }: Props) {
   return (
     <div>
       <label
@@ -11,11 +15,11 @@ export default function Select() {
         id="location"
         name="location"
         className="mt-2 block w-full rounded-md border-0 py-1.5 pl-3 pr-10 text-gray-900 ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-indigo-600 sm:text-sm sm:leading-6"
-        defaultValue="Canada"
+        defaultValue="All Locations"
       >
-        <option>United States</option>
-        <option>Canada</option>
-        <option>Mexico</option>
+        {distinctLocations.map((location) => (
+          <option key={location}>{location}</option>
+        ))}
       </select>
     </div>
   );
